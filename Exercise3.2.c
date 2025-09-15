261b7c5e34b531b6306c9a66c743d9e5f9a8043e
LatteExtraThick <sidpro2010@gmail.com>
2025-09-16 03:03:09 +0530

    Add files via upload

---
+++ Exercise3.2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int year;
+    printf("Enter a year: ");
+    scanf("%d", &year);
+
+    (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)
+        ? printf("%d is a Leap Year\n", year)
+        : printf("%d is Not a Leap Year\n", year);
+
+    return 0;
+}
